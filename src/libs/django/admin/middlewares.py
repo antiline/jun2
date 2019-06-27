afe8100e5e5fef97c76063e8c6a10eef3badf411
antiline <>
2019-06-28 00:08:45 +0900

    Admin 접근 안되는 버그 수정 - 오타 수정

--- src/libs/django/admin/middlewares.py
+++ src/libs/django/admin/middlewares.py
@@ -10,5 +10,5 @@ class AdminIPRestrictorMiddleware(BaseAdminIPRestrictorMiddleware):
         self.allowed_admin_proxy_count = getattr(settings, 'ALLOWED_ADMIN_PROXY_COUNT', 0)
 
     def get_ip(self, request):
-        client_ip, _ = get_client_ip(request, settings.allowed_admin_proxy_count)
+        client_ip, _ = get_client_ip(request, self.allowed_admin_proxy_count)
         return client_ip
