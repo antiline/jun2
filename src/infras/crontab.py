7cae29a6139664825c7d97b17a2fa326619ec782
antiline <>
2019-07-01 21:06:33 +0900

    crawl_gpx 를 5분마다 실행하도록 함

--- src/infras/crontab.py
+++ src/infras/crontab.py
@@ -5,7 +5,7 @@ from infras.constants.lock_constants import FileLockKeyName
 from libs.lock.decorators import f_lock
 
 
-@cron(0, -1, -1, -1, -1)
+@cron(-5, -1, -1, -1, -1)
 @f_lock(FileLockKeyName.CRAWL_GPX, lock_ttl=3600*24)
 def crawl_gpx(signum: int):
     call_command('crawl_gpx')
